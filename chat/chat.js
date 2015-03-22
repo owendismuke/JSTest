94646bba6b11174f760f2de99f23b27fcb73bb85
Owen Dismuke <owen.dismuke@gmail.com>
2015-03-21 23:01:28 -0700

    Angularity the firebase chat tutorial.
    Still need to pretty the page, make this better, and implement
    scrolling.

--- chat/chat.js
+++ chat/chat.js
@@ -9,21 +9,18 @@ angular.module( 'owen.chat', [
     input: "",
     submit: function(){
       message.push({user: auth.profile.name, message: this.input, email: auth.profile.email || "No email provided."});
+      this.input = "";
     }
   };
+  $scope.data = {
+    messages: []
+  };
 
-  //Currently copied directly from Firebase tutorial
+  
   message.on('child_added', function(snapshot){
     var message = snapshot.val();
-    displayMessage(message.name, message.text);
+    $scope.data.messages.push(message.name + ': ' + message.text)
   });
-
-  function displayMessage(name, text) {
-    //This won't work because no jQuery. Change to plain js or angular.
-        $('<div/>').text(text).prepend($('<em/>').text(name+': ')).appendTo($('#messages'));
-        $('#messages')[0].scrollTop = $('#messages')[0].scrollHeight;
-      };
-
 
   $scope.logout = function() {
     auth.signout();
